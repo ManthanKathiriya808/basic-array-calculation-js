55d1324e0851f66863f5bf45e89497265395f3d0
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-19 16:02:28 +0530

    asdg

---
+++ array/array.js
@@ -0,0 +1,30 @@
+
+
+let arr= [1,2,3,9,5,6,7,8]
+
+// arr.push(9)
+// arr.push(10)
+// arr.pop()
+// arr.pop()
+
+// arr.sort()
+
+// arr.shift()
+// arr.unshift(5)
+// arr.unshift(1)
+// arr.shift()
+// console.log(arr)
+// let arr1 = arr.slice(3,5)
+// console.log(arr1)
+// console.log(arr)
+// let arr2 = arr.splice(3,4)
+// console.log(arr2)
+
+
+console.log(arr.indexOf(6))
+console.log(arr.length)
+
+
+let arr1 =  [1,2,3,4,5]
+
+console.log(arr1.join("-"))
---
+++ array/new.js
@@ -0,0 +1,10 @@
+let snacks = [" samosa", "vadapav"]
+let sweet = [" chocolate", "jalebi"]
+let light = [" fafde", "gathya"]
+
+
+// let big = snacks.concat(sweet)
+// console.log(big)
+
+
+// console.log(...snacks , ...sweet,...light)
